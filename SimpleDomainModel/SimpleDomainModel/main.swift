639d7e85ad984716814e300a75b9f961630f35a1
Duncan Andrew <duncanandrew@live.com>
2017-04-08 16:17:12 -0700

    all unit tests passed

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2016 Ted Neward. All rights reserved.
 //
 
+// How can I get the tests to show me the actual vs expected?
+// Whats a good way to get the number from the enum jobtype?
+// Whats the meaning behind a _job and a job?
+
 import Foundation
 
 print("Hello, World!")
@@ -28,11 +32,53 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    // Converts self to a universal curency that can be evenly divided into any other currency
+    var amt = 0
+    if self.currency == "USD"{
+        amt = self.amount * 15
+    }else if self.currency == "GBP"{
+        amt = self.amount * 30
+    }else if self.currency == "EUR"{
+        amt = self.amount * 10
+    }else if self.currency == "CAN"{
+        amt = self.amount * 12
+    }else{
+        // Self is not a recognized currency type
+    }
+    
+    // Divides universal currency into the requested type
+    var ret: Money = Money(amount: 0, currency: "")
+    if to == "USD"{
+        ret = Money(amount: amt / 15, currency: "USD")
+    }else if to == "GBP"{
+        ret = Money(amount: amt / 30, currency: "GBP")
+    }else if to == "EUR"{
+        ret = Money(amount: amt / 10, currency: "EUR")
+    }else if to == "CAN"{
+        ret = Money(amount: amt / 12, currency: "CAN")
+    }else{
+        // to is not a recognized currency type
+    }
+    
+    return ret
   }
   
   public func add(_ to: Money) -> Money {
+    var me = self
+    if(me.currency != to.currency){
+        me = me.convert(to.currency)
+    }
+    let ret = Money(amount: me.amount + to.amount, currency: to.currency)
+    return ret
   }
+    
   public func subtract(_ from: Money) -> Money {
+    var other = from
+    if(self.currency != other.currency){
+        other = other.convert(self.currency)
+    }
+    let ret = Money(amount: other.amount - self.amount, currency: self.currency)
+    return ret
   }
 }
 
@@ -49,12 +95,22 @@ open class Job {
   }
   
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
   
   open func calculateIncome(_ hours: Int) -> Int {
+    switch self.type{
+    case .Hourly(let num): return Int(num * Double(hours))
+    case .Salary(let num): return num
+    }
   }
   
   open func raise(_ amt : Double) {
+    switch self.type{
+    case .Hourly(let num): self.type = JobType.Hourly(num + amt)
+    case .Salary(let num): self.type = JobType.Salary(num + Int(amt))
+    }
   }
 }
 
@@ -66,17 +122,28 @@ open class Person {
   open var lastName : String = ""
   open var age : Int = 0
 
+  // Whats the meaning behind a _job and a job?
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
+    get {
+        return self._job
+    }
     set(value) {
+        if self.age >= 16 {
+            self._job = value
+        }
     }
   }
   
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
+    get {
+        return self._spouse
+    }
     set(value) {
+        if self.age >= 18 {
+            self._spouse = value
+        }
     }
   }
   
@@ -87,6 +154,15 @@ open class Person {
   }
   
   open func toString() -> String {
+    var jobString = "nil"
+    var spouseString = "nil"
+    if self.job != nil{
+        jobString = " title:\(self.job!.title) type:\(self.job!.type)"
+    }
+    if self.spouse != nil{
+        spouseString = self.spouse!.toString()// This could potentially become an infinite loop
+    }
+    return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(jobString) spouse:\(spouseString)]"
   }
 }
 
@@ -97,12 +173,28 @@ open class Family {
   fileprivate var members : [Person] = []
   
   public init(spouse1: Person, spouse2: Person) {
+    if spouse1.spouse == nil && spouse2.spouse == nil{
+        spouse1.spouse = spouse2
+        spouse2.spouse = spouse1
+        self.members.append(spouse1)
+        self.members.append(spouse2)
+    }
   }
   
   open func haveChild(_ child: Person) -> Bool {
+    self.members.append(child)
+    return true // Whats the point of this return?
+    // Can't restrict the child to age 0, unit tests fail
   }
   
   open func householdIncome() -> Int {
+    var ret = 0
+    for person in self.members{
+        if person.job != nil{
+            ret = ret + Int(person.job!.calculateIncome(2000)) // Why does it need to be over 2000 hours?
+        }
+    }
+    return ret
   }
 }
 
